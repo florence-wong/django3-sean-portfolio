516f0f4fa1ddd5e77d1ac8e595c89ee0cafe4e74
florence-wong <118371119+florence-wong@users.noreply.github.com>
2024-02-19 10:45:21 -0500

    added article TBS

--- publications/models.py
+++ publications/models.py
@@ -9,6 +9,7 @@ class Publication(models.Model):
     #description=models.CharField(max_length=450)
     image=models.ImageField(upload_to="portfolio/images/")
     url=models.URLField(blank=True)
+    date=models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.title
